c86b74b2b266ad53921dce0dca7c1c6529b807b4
Uta Stefan <stefanuta82@gmail.com>
2025-02-22 15:16:27 +0200

    [forgot import useEffect]

--- src/components/Led/Led.jsx
+++ src/components/Led/Led.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import 'primereact/resources/primereact.min.css';
 import 'primeicons/primeicons.css';
 import { Knob } from 'primereact/knob';
